3cfbf3debd58993eb2cb61181ea94f7c0e5a94f3
Kuznetcov Dmitriy <d.kuznetcov@gmail.com>
2019-03-12 20:19:30 +0300

    d
    dfg

--- ajax/exit.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-setcookie('log', $login, time() - 3600 * 24 * 30, "/bonds");
-echo true;
-
-?>
--- blocks/head.php
+++
@@ -1,9 +0,0 @@
- <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta http-equiv="X-UA-Compatible" content="ie-edge">
-    <title><?=$website_title?></title>
-    <link rel="stylesheet" href="/bonds/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/bonds/css/main.css">
-    <!--<link rel="stylesheet" href="/bonds/js/jquery.min.js">  -->
-    
-    <link rel="icon" href="/bonds/img/favicon.ico">
